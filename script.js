de1668143376d652f055cb90422b8ed614054a2a
mikespyridon <120049095+mikespyridon@users.noreply.github.com>
2023-11-23 12:13:04 -0500

    Add tools panel and ability to change draw size

--- script.js
+++ script.js
@@ -1,20 +1,47 @@
 const frame = document.querySelector('.window');
 const clearBtn = document.querySelector('.btn-container')
+const bigBtn = document.querySelector('.big');
+const smallBtn = document.querySelector('.small');
+const colorBtn = document.querySelector('.color');
+const eraseBtn = document.querySelector('.erase');
 
+const bigBlock = 256;
+const smallBlock = 1024;
+
+bigBtn.addEventListener('click', () => {
+  clearWindow();
+  createGrid(bigBlock);
+});
+
+smallBtn.addEventListener('click', () => {
+  clearWindow();
+  createGrid(smallBlock);
+})
 
 function createGrid(size) {
-  for (let i = 1; i <= size; i += 1) {
-    const block = document.createElement('div');
-    block.classList.add('block')
-    frame.appendChild(block);
-  
-    block.addEventListener('mousemove', () => {
-      block.classList.add('fill');
-    });
+  if (size === bigBlock) {
+    for (let i = 1; i <= bigBlock; i += 1) {
+      const block = document.createElement('div');
+      block.classList.add('big-block')
+      frame.appendChild(block);
+
+      block.addEventListener('mousemove', () => {
+        block.classList.add('fill');
+      });
+    }
+  } else if (size === smallBlock) {
+    for (let i = 1; i <= smallBlock; i += 1) {
+      const block = document.createElement('div');
+      block.classList.add('small-block')
+      frame.appendChild(block);
+
+      block.addEventListener('mousemove', () => {
+        block.classList.add('fill');
+      });
+    }
   }
 }
 
-createGrid(256);
 
 function clearWindow() {
   frame.textContent = "";  
