388e5eacfa812ee583a1e403bdbdb49b003646ae
mikespyridon <120049095+mikespyridon@users.noreply.github.com>
2023-11-24 09:33:31 -0500

    Add medium size option and rainbow color option

--- script.js
+++ script.js
@@ -1,53 +1,109 @@
 const frame = document.querySelector('.window');
 const clearBtn = document.querySelector('.btn-container')
 const bigBtn = document.querySelector('.big');
+const mediumBtn = document.querySelector('.medium');
 const smallBtn = document.querySelector('.small');
 const colorBtn = document.querySelector('.color');
 const eraseBtn = document.querySelector('.erase');
+const fillColor = document.querySelector('.fill');
 
-const bigBlock = 256;
-const smallBlock = 1024;
+const bigBlock = {
+  size: 256,
+  clicked: false
+}
 
-bigBtn.addEventListener('click', () => {
-  clearWindow();
-  createGrid(bigBlock);
-});
+const mediumBlock = {
+  size: 1024,
+  clicked: false
+}
 
-smallBtn.addEventListener('click', () => {
-  clearWindow();
-  createGrid(smallBlock);
-})
+const smallBlock = {
+  size: 4096,
+  clicked: false,
+}
+
+let clickedRainbow = false;
+
+function fillGrid(block) {
+  if (clickedRainbow) {
+    block.style.backgroundColor = randomColor();
+    block.classList.remove('fill-black');
+    block.classList.add('fill-rainbow');
+  } else if (!clickedRainbow) {
+    block.style.backgroundColor = 'black';
+    block.classList.remove('fill-rainbow');
+    block.classList.add('fill-black');
+  }
+}
 
 function createGrid(size) {
-  if (size === bigBlock) {
-    for (let i = 1; i <= bigBlock; i += 1) {
+  if (size === bigBlock.size) {
+    for (let i = 1; i <= bigBlock.size; i += 1) {
       const block = document.createElement('div');
       block.classList.add('big-block')
       frame.appendChild(block);
 
-      block.addEventListener('mousemove', () => {
-        block.classList.add('fill');
-      });
+      block.addEventListener('mouseenter', () => {
+        fillGrid(block);
+      }, true);
     }
-  } else if (size === smallBlock) {
-    for (let i = 1; i <= smallBlock; i += 1) {
+  } else if (size === mediumBlock.size) {
+    for (let i = 1; i <= mediumBlock.size; i += 1) {
+      const block = document.createElement('div');
+      block.classList.add('medium-block')
+      frame.appendChild(block);
+
+      block.addEventListener('mouseenter', () => {
+        fillGrid(block);
+      }, true);
+    }
+  } else if (size === smallBlock.size) {
+    for (let i = 1; i <= smallBlock.size; i += 1) {
       const block = document.createElement('div');
       block.classList.add('small-block')
       frame.appendChild(block);
 
-      block.addEventListener('mousemove', () => {
-        block.classList.add('fill');
-      });
+      block.addEventListener('mouseenter', () => {
+        fillGrid(block);
+      }, true);
     }
   }
 }
 
+function randomColor() {
+  let r = Math.floor(Math.random() * 256);
+  let g = Math.floor(Math.random() * 256);
+  let b = Math.floor(Math.random() * 256);
 
+  return `rgb(${r}, ${g}, ${b})`;
+}
 function clearWindow() {
   frame.textContent = "";  
 }
 
 clearBtn.addEventListener('click', () => {
   clearWindow();
-  createGrid(256);
+});
+
+bigBtn.addEventListener('click', () => {
+  clearWindow();
+  createGrid(bigBlock.size);
+});
+
+mediumBtn.addEventListener('click', () => {
+  clearWindow();
+  createGrid(mediumBlock.size);
+});
+
+smallBtn.addEventListener('click', () => {
+  clearWindow();
+  createGrid(smallBlock.size);
+});
+
+colorBtn.addEventListener('click', () => {
+  if (clickedRainbow) {
+    clickedRainbow = false;
+  } else {
+    clickedRainbow = true;
+  }
 });
